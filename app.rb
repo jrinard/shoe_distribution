533440d870e3b0de99176e06d79328596a47f68c
Joshua Rinard <jrinard@hotmail.com>
2016-12-16 09:43:26 -0800

    basic routing, successfully create a store and a brand in via form

--- app.rb
+++ app.rb
@@ -6,6 +6,57 @@ require("pg")
 require('./lib/store')
 require('./lib/brand')
 
+#home route
 get('/') do
+  @brands = Brand.all #grabs all the brands and makes them available
   erb(:index)
+end
+
+#store - multiple
+get('/stores') do
+  @stores = Store.all #grabs all the stores and makes them available
+  erb(:stores)
+end
+
+#store - single
+get('/store/:id') do
+  @stores = Store.find(params.fetch("id").to_i) #finding single recipe with that id
+  @brands = @stores.brands
+  @allbrands = Brand.all
+  erb(:store_detail)
+end
+
+#create recipe
+post('/stores') do
+  new_name = params['store-name'] # [] are the same as .fetch but default nil or null
+  new_address = params['address']
+  new_cost = params['cost']
+  new_rating = params['rating']
+  new_brand = Brand.find(params['brand_id'].to_i)
+  #creates a new store with the data it fetched above
+  @stores = Store.new({:name => new_name, :address => new_address, :cost => new_cost, :ratings => new_rating})
+  if @stores.save
+      #pushes new brand to the store you just created
+      @stores.brands.push(new_brand)
+      redirect "/recipes"
+  else
+    erb(:errors)
+  end
+end
+
+
+
+
+
+
+#create brand
+post('/brands') do
+  new_brand = params['brand']
+  #create a new brand with the value in the form and namt it @brand
+  @brand = Brand.new({:name => new_brand})
+  if @brand.save
+    redirect "/"
+  else
+    erb(:errors2)
+  end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,3 +2,28 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+
+describe('Main Route', {:type => :feature}) do
+  describe('Create Brand') do
+    it "allows user to create a brand" do
+      visit('/')
+      fill_in('brand', :with => "Crocs")
+      click_button('Create Brand')
+      visit('/brands')
+      expect(page).to have_content 'Crocs'
+  end
+    it "allows the user to create a store" do
+      visit('/')
+      fill_in('new-name', :with => "Foot Locker")
+      fill_in('new-address', :with => "1 Infinite Loop")
+      fill_in('new-cost', :with => "$20")
+      fill_in('new-rating', :with => "5")
+      fill_in('brand', :with => "Crocs")
+      click_button('Create Store')
+      visit('/stores')
+      expect(page).to have_content 'Store List'
+    end
+  end
+
+end

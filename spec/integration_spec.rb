8520d515ccf6098681c08f50ebb364939e3210f6
Joshua Rinard <jrinard@hotmail.com>
2016-12-16 10:21:16 -0800

    spec for store routing

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -13,16 +13,31 @@ describe('Main Route', {:type => :feature}) do
       visit('/brands')
       expect(page).to have_content 'Crocs'
   end
-    it "allows the user to create a store" do
-      visit('/')
-      fill_in('new-name', :with => "Foot Locker")
-      fill_in('new-address', :with => "1 Infinite Loop")
-      fill_in('new-cost', :with => "$20")
-      fill_in('new-rating', :with => "5")
-      fill_in('brand', :with => "Crocs")
-      click_button('Create Store')
-      visit('/stores')
-      expect(page).to have_content 'Store List'
+    # it "allows the user to create a store" do
+    #   visit('/')
+    #   fill_in('store-name', :with => "Foot Locker")
+    #   fill_in('new-address', :with => "1 Infinite Loop")
+    #   fill_in('new-rating', :with => "5")
+    #   fill_in('brand', :with => "Crocs")
+    #   click_button('Create Store')
+    #   visit('/stores')
+    #   expect(page).to have_content 'Store List'
+    # end
+  end
+
+  describe('show a store') do
+    it "allows user to view individual store" do
+      Store.create({:name => 'The Crocs Store', :address => '1 Infinite Loop', :ratings => 4})
+      visit("/stores")
+      expect(page).to have_content 'The Crocs Store'
+    end
+  end
+
+  describe('show a individual store') do
+    it "allows user to view individual store" do
+      store = Store.create({:name => 'The Crocs Store', :address => '1 Infinite Loop', :ratings => 4})
+      visit("/store/#{store.id}")
+      expect(page).to have_content 'The Crocs Store'
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,11 @@ require('./lib/store')
 require('./lib/brand')
 
 RSpec.configure do |config|
-  config.after(:each) do
-    Store.all.each(&:destroy)
-    Brand.all.each(&:destroy)
+  # config.after(:each) do
+  #   Store.all.each(&:destroy)
+  #   Brand.all.each(&:destroy)
+config.before(:each) do
+    Store.destroy_all
+    Brand.destroy_all
   end
 end
